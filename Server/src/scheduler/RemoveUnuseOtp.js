a98d7b2494c9aec0346ede226b61636a731e39db
Dipronil1998 <dipronilda.net@gmail.com>
2022-09-25 20:17:36 +0530

    scheduling bug fixed

--- Server/src/scheduler/RemoveUnuseOtp.js
+++ Server/src/scheduler/RemoveUnuseOtp.js
@@ -5,11 +5,12 @@ const logger = require('../utils/logger');
 
 exports.otpCron = cron.schedule('* * * * *', async () => {
   try {
-    const previousTime = moment(new Date()).subtract(15, 'days')
+    const previousTime = moment(new Date()).subtract(7, 'days')
         .format('YYYY-MM-DD hh:mm:ss');
     const otp = await Otp
-        .deleteMany({createdAt: {$lte: new Date(new Date(previousTime)
-            .toISOString())}});
+        .deleteMany({
+          createdAt: {$lte: new Date(new Date(previousTime).toISOString())},
+        });
     if (otp.deletedCount > 0) {
       console.log(otp.deletedCount + ' OTP deleted');
       logger.info(otp.deletedCount + ' OTP deleted');
