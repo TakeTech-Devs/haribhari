832fbe7ccc42447155a9b6aa305ebeb5bb35b579
Dipronil1998 <dipronilda.net@gmail.com>
2022-10-18 06:26:44 +0530

    bug fixed in express-validator

--- Server/src/middleware/ValidateResult.js
+++ Server/src/middleware/ValidateResult.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 exports.validateResult=(req, res, next)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        if (req.files.length > 0) {
+        // if (req.files.length > 0) {
+        //     req.files.forEach((element) => {
+        //         fs.unlinkSync(element.path);
+        //     });
+        // }
+        if(req.files){
+            console.log("AA");
             req.files.forEach((element) => {
                 fs.unlinkSync(element.path);
             });
